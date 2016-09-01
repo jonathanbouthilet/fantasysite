aab36bfd4360111fde7de05e0fcd37e30e4f1f5c
Jonathan Bouthilet <jonathanbouthilet@gmail.com>
2016-09-01 18:58:48 +0000

    Added team buttons with points

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	 $.ajax({
-        url: "https://sheets.googleapis.com/v4/spreadsheets/1akhr89xy3Ka0uU__DFQwFOiX9LWSEief_TtuR-g685o/values/A1%3AE128?key=AIzaSyAu87BNGJXdihEYsIOEjI8pMU9dntFcd3o",
+        url: "https://sheets.googleapis.com/v4/spreadsheets/1akhr89xy3Ka0uU__DFQwFOiX9LWSEief_TtuR-g685o/values/A1%3AE129?key=AIzaSyAu87BNGJXdihEYsIOEjI8pMU9dntFcd3o",
         // Handle as Text
                 
         dataType: "text",
@@ -74,6 +74,37 @@ $(document).ready(function(){
                           
             } //end table loop
            
+            
+             //Team Buttons        
+         
+             
+             $('.button-one').append("<tr><td>" + obj.values[1][0] + " " + obj.values[16][4] + "</td></tr>" );
+             $('.button-one').append("<tr><td>" + obj.values[17][0] + " " + obj.values[32][4] + "</td></tr>" );
+             $('.button-one').append("<tr><td>" + obj.values[33][0] + " " + obj.values[48][4] + "</td></tr>" );
+             $('.button-one').append("<tr><td>" + obj.values[49][0] + " " + obj.values[64][4] + "</td></tr>" );
+             
+             $('.button-two').append("<tr><td>" + obj.values[65][0] + " " + obj.values[80][4] + "</td></tr>" );
+             $('.button-two').append("<tr><td>" + obj.values[81][0] + " " + obj.values[96][4] + "</td></tr>" );
+             $('.button-two').append("<tr><td>" + obj.values[97][0] + " " + obj.values[112][4] + "</td></tr>" );
+             $('.button-two').append("<tr><td>" + obj.values[113][0] + " " + obj.values[128][4] + "</td></tr>" );
+             
+             $('.button-three').append("<tr><td>" + obj.values[1][0] + " " + obj.values[16][4] + "</td></tr>" );
+             $('.button-three').append("<tr><td>" + obj.values[17][0] + " " + obj.values[32][4] + "</td></tr>" );
+             $('.button-three').append("<tr><td>" + obj.values[33][0] + " " + obj.values[48][4] + "</td></tr>" );
+             $('.button-three').append("<tr><td>" + obj.values[49][0] + " " + obj.values[64][4] + "</td></tr>" );
+             
+             $('.button-four').append("<tr><td>" + obj.values[1][0] + " " + obj.values[16][4] + "</td></tr>" );
+             $('.button-four').append("<tr><td>" + obj.values[17][0] + " " + obj.values[32][4] + "</td></tr>" );
+             $('.button-four').append("<tr><td>" + obj.values[33][0] + " " + obj.values[48][4] + "</td></tr>" );
+             $('.button-four').append("<tr><td>" + obj.values[49][0] + " " + obj.values[64][4] + "</td></tr>" );
+            
+             
+             
+                         
+                         
+            
+            
+            
                
         }
     
